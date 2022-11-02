11e18654f1a28208f931bfb2c5dbaccc2bb6f44d
seondal <sseondal@gmail.com>
2022-11-02 17:25:44 +0900

    #1.0 Vanilla Counter

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count++;
+  updateText();
+};
+
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+plus.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
