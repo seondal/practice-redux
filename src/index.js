7a161e7686cc2f886191b074b3edbb59f1c56ed9
seondal <sseondal@gmail.com>
2022-11-07 18:48:49 +0900

    #1.2-#1.3 Actions & Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countStoreModifier = (state = 0) => {
-  return "hello";
+const countStoreModifier = (count = 0, action) => {
+  if (action.type === "PLUS") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countStoreModifier);
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+const handlePlus = () => {
+  countStore.dispatch({ type: "PLUS" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
 
 console.log(countStore.getState());
